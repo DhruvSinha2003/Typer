402ca4925e133eb670f63a75638eeda865bdae24
DhruvSinha2003 <dhruv.sinha06@gmail.com>
2024-04-04 19:31:18 +0530

    Wordlist working

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-
+const {wordList, generateRandomWordList} = require('./components/Wordlist');
 function App() {
   return (
     <div className="App">
-      
+      {generateRandomWordList(5).map((word, index) => (
+        <div key={index}>{word}</div>
+      ))}
     </div>
   );
 }
--- src/components/Wordlist.js
+++ src/components/Wordlist.js
@@ -1,4 +1,34 @@
-const wordListPath = require('word-list'); 
-const wordArray = fs.readFileSync(wordListPath, 'utf8').split('\n');
+// wordList.js
 
-export default wordArray;
+const wordList = [
+    "apple",
+    "banana",
+    "orange",
+    "grape",
+    "pear",
+    // Add more words as needed
+  ];
+  
+  // Function to generate a random integer between min (inclusive) and max (inclusive)
+  function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+  
+  // Function to generate a random list of words of specified length
+  function generateRandomWordList(length) {
+    const randomWordList = [];
+    const wordListLength = wordList.length;
+  
+    for (let i = 0; i < length; i++) {
+      const randomIndex = getRandomInt(0, wordListLength - 1);
+      randomWordList.push(wordList[randomIndex]);
+    }
+  
+    return randomWordList;
+  }
+  
+  module.exports = {
+    wordList,
+    generateRandomWordList
+  };
+  
